3a747b657948d95ea5aed834d0f856a10d97633a
duraes-antonio <garotoseis@gmail.com>
2022-09-18 13:02:54 -0300

    REFACTOR: Adicionar tipagem ao métodos de particionamento de data e generator

---
+++ domain/models/data/data_generator.py
@@ -0,0 +1,64 @@
+from typing import Tuple, List, Iterable, Union
+
+import numpy
+from keras.utils import Sequence
+from numpy import ndarray, arange, floor
+
+from domain.util.preprocessing import preprocess_image, preprocess_depth_map
+
+
+# from tensorflow.keras.utils import Sequence
+
+
+class NyuV2Generator(Sequence):
+    shuffle: bool
+    is_depth_map: bool
+    batch_size: int
+    n_channels: int
+    image_size: Tuple[int, int] = (640, 480)
+    indexes: ndarray
+
+    def __init__(self, path_list: List[str], labels, batch_size=4, n_channels=3, shuffle=True, is_depth_map=False, seed=42):
+        self.n_channels = n_channels
+        self.batch_size = batch_size
+        self.is_depth_map = is_depth_map
+        self.labels = labels
+        self.paths = path_list
+        self.indexes = arange(len(path_list))
+        self.length = int(floor(len(path_list) / batch_size))
+
+        self.shuffle = shuffle
+        self.seed = seed
+        self.on_epoch_end()
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index: int) -> Union[ndarray, Tuple[ndarray, ndarray]]:
+        start_index_batch = index * self.batch_size
+        end_index_batch = (index + 1) * self.batch_size
+        indexes = self.indexes[start_index_batch: end_index_batch]
+        list_ids_temp = [self.paths[i] for i in indexes]
+        return self.__data_generation__(list_ids_temp)
+
+    def on_epoch_end(self):
+        self.indexes = arange(len(self.paths))
+
+        if self.shuffle:
+            numpy.random.shuffle(self.indexes)
+
+    def __data_generation__(self, paths: Iterable[str]) -> Union[ndarray, Tuple[ndarray, ndarray]]:
+        width, height = self.image_size
+        empty_batch = numpy.empty((self.batch_size, width, height, self.n_channels))
+
+        if self.is_depth_map:
+            for index, target_path in enumerate(paths):
+                empty_batch[index,] = preprocess_image(target_path)
+            return empty_batch
+
+        target_depth_map = numpy.empty((self.batch_size, width, height, 1))
+
+        for index, target_path in enumerate(paths):
+            empty_batch[index,] = preprocess_image(target_path)
+            target_depth_map[index,] = preprocess_depth_map(self.labels[target_path])
+        return empty_batch, target_depth_map
--- domain/util/dataset.py
+++ domain/util/dataset.py
@@ -1,30 +1,45 @@
 import csv
-import random
+from typing import List, Tuple, Dict, MutableSequence, TypedDict
 
 
-def read_csv(csv_file_path):
-    with open(csv_file_path, 'r') as f:
-        csv_reader = csv.reader(f, delimiter=',')
+class PartitionedDataset(TypedDict):
+    train: MutableSequence[str]
+    validation: MutableSequence[str]
+
+
+def read_nyu_csv(csv_file_path) -> List[Tuple[str, str]]:
+    with open(csv_file_path, 'r') as file:
+        csv_reader = csv.reader(file, delimiter=',')
         return [('./' + row[0], './' + row[1]) for row in csv_reader if len(row) > 0]
 
 
-def train_val_split(train_paths, val_size):
-    random.shuffle(train_paths)
-    len_train_paths = len(train_paths)
-    i = int(len_train_paths * (1.0 - val_size))
-    train = train_paths[0:i]
-    val = train_paths[i:len(train_paths)]
-    return train, val
+def split_train_validation(
+        paths_to_split: MutableSequence[str],
+        val_percent: float,
+        seed: int
+) -> Tuple[MutableSequence[str], MutableSequence[str]]:
+    import random
+    random.seed(seed)
+    random.shuffle(paths_to_split)
+
+    count_all_paths = len(paths_to_split)
+    count_train_paths = int(count_all_paths * (1.0 - val_percent))
+
+    train_paths = paths_to_split[:count_train_paths]
+    validation_paths = paths_to_split[count_train_paths:]
+    return train_paths, validation_paths
 
 
-def load_train_paths(train_path):
-    train_paths = read_csv(train_path)
-    labels = {img_path: dm_path for img_path, dm_path in train_paths}
-    x_paths = [img_path for img_path, dm in train_paths]
-    x_train_paths, x_val_paths = train_val_split(x_paths, 0.3)
+def load_nyu_train_paths(
+        train_csv_path: str,
+        val_percent: 0.3,
+        seed: int
+) -> Tuple[PartitionedDataset, Dict[str, str]]:
+    xy_paths_pairs = read_nyu_csv(train_csv_path)
+    y_path_by_x_path: Dict[str, str] = {x_path: y_path for x_path, y_path in xy_paths_pairs}
 
-    partition = {
-        'train': x_train_paths,
-        'validation': x_val_paths
-    }
-    return partition, labels
+    x_paths = [x_path for x_path, y_path in xy_paths_pairs]
+    x_train_paths, x_val_paths = split_train_validation(x_paths, val_percent, seed)
+
+    partition = PartitionedDataset(train=x_train_paths, validation=x_val_paths)
+    return partition, y_path_by_x_path
