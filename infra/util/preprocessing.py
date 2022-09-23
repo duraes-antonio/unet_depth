670ab20b5a0e91db20813f2f40833a66d9283c3e
duraes-antonio <garotoseis@gmail.com>
2022-09-22 21:37:49 -0300

    REFACTOR: Substituir imutils por opencv

--- infra/util/preprocessing.py
+++ infra/util/preprocessing.py
@@ -1,29 +1,28 @@
 import cv2
-import imutils
 from numpy import ndarray, reshape
 
 DEFAULT_HEIGHT = 256
+DEFAULT_SIZE = (DEFAULT_HEIGHT, DEFAULT_HEIGHT)
 
 
 def normalize_img(img: ndarray) -> ndarray:
     return (img - img.min()) / (img.max() - img.min())
 
 
-def preprocess_image(img_path: str, image_shape=(640, 480)) -> ndarray:
+def preprocess_image(img_path: str) -> ndarray:
     image: ndarray = cv2.imread(img_path)
-    image = imutils.resize(image, height=DEFAULT_HEIGHT)
+    image = cv2.resize(image, DEFAULT_SIZE, interpolation=cv2.INTER_AREA)
     image = image.astype("float")
     image = normalize_img(image)
     return image
 
 
-def preprocess_depth_map(depth_map_path: str, image_shape=(640, 480)) -> ndarray:
+def preprocess_depth_map(depth_map_path: str) -> ndarray:
     depth_map: ndarray = cv2.imread(depth_map_path)
     depth_map = cv2.cvtColor(depth_map, cv2.COLOR_BGR2GRAY)
-    depth_map = imutils.resize(depth_map, height=DEFAULT_HEIGHT)
-    width, height = image_shape
+    depth_map = cv2.resize(depth_map, DEFAULT_SIZE, interpolation=cv2.INTER_AREA)
 
     depth_map = depth_map.astype("float")
     depth_map = normalize_img(depth_map)
-    depth_map = reshape(depth_map, (height, width, 1))
+    depth_map = reshape(depth_map, (DEFAULT_HEIGHT, DEFAULT_HEIGHT, 1))
     return depth_map
