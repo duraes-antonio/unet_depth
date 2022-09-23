23f6853644cc494ad470ed1c7b71c53cd2e08ff6
duraes-antonio <garotoseis@gmail.com>
2022-09-22 21:18:00 -0300

    FIX: Converter entrada e label para 256 pixels de altura e largura

--- infra/util/preprocessing.py
+++ infra/util/preprocessing.py
@@ -1,13 +1,17 @@
 import cv2
+import imutils
 from numpy import ndarray, reshape
+
+DEFAULT_HEIGHT = 256
 
 
 def normalize_img(img: ndarray) -> ndarray:
     return (img - img.min()) / (img.max() - img.min())
 
 
-def preprocess_image(img_path: str) -> ndarray:
+def preprocess_image(img_path: str, image_shape=(640, 480)) -> ndarray:
     image: ndarray = cv2.imread(img_path)
+    image = imutils.resize(image, height=DEFAULT_HEIGHT)
     image = image.astype("float")
     image = normalize_img(image)
     return image
@@ -16,6 +20,7 @@ def preprocess_image(img_path: str) -> ndarray:
 def preprocess_depth_map(depth_map_path: str, image_shape=(640, 480)) -> ndarray:
     depth_map: ndarray = cv2.imread(depth_map_path)
     depth_map = cv2.cvtColor(depth_map, cv2.COLOR_BGR2GRAY)
+    depth_map = imutils.resize(depth_map, height=DEFAULT_HEIGHT)
     width, height = image_shape
 
     depth_map = depth_map.astype("float")
