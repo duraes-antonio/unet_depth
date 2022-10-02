1dddca6b8e43097cdc6c0723a89fe61c01b042b6
duraes-antonio <garotoseis@gmail.com>
2022-10-02 02:06:53 -0300

    FEAT: Permitir renomear arquivo de modelo treinado antes de salvá-lo

--- domain/services/model_storage_service.py
+++ domain/services/model_storage_service.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 
 class ModelStorageService(ABC):
 
     @abstractmethod
-    def save(self, file_path: str) -> str:
+    def save(self, file_path: str, new_filename: Optional[str]) -> str:
         """
         Prepare and store a trained model
+        :param new_filename: File name after stored
         :param file_path: Path of the file to be stored
         :return: Stored file id
         """
--- infra/services/model_storage_service_google_drive.py
+++ infra/services/model_storage_service_google_drive.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from pathlib import Path
+from typing import Optional
 
 from domain.services.blob_storage_service import BlobStorageService
 from domain.services.model_storage_service import ModelStorageService
@@ -11,10 +12,10 @@ class ModelStorageServiceGoogleDrive(ModelStorageService):
     def __init__(self, storage_service: BlobStorageService):
         self.storage_service = storage_service
 
-    def save(self, file_path: str) -> str:
+    def save(self, file_path: str, new_filename: Optional[str]) -> str:
         current_path = Path(file_path)
 
-        zip_filename = current_path.name
+        zip_filename = new_filename or current_path.name
         shutil.make_archive(zip_filename, 'zip', file_path)
 
         zip_complete_path = f'{zip_filename}.zip'
