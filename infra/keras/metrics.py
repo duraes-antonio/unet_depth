9e252422f3112d86d7fe941603f7ae730c3dc270
duraes-antonio <garotoseis@gmail.com>
2022-10-05 00:41:06 -0300

    REFACTOR: Normalizar valores para acima de 1e-10

--- infra/keras/metrics.py
+++ infra/keras/metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow
+import tensorflow as tf
 import tensorflow.keras.backend as k_backend
 from numpy import inf
 from tensorflow import Tensor
@@ -47,8 +47,8 @@ def threshold_3(ground_truth: Tensor, predicted: Tensor):
 
 
 def abs_rel(ground_truth: Tensor, predicted: Tensor):
-    gt_np = np.array(tensorflow.get_static_value(ground_truth))
-    pred_np = np.array(tensorflow.get_static_value(ground_truth))
+    gt_np = np.array(tf.get_static_value(ground_truth))
+    pred_np = np.array(tf.get_static_value(ground_truth))
 
     if inf in gt_np:
         print('\nground_truth:', gt_np)
@@ -58,7 +58,7 @@ def abs_rel(ground_truth: Tensor, predicted: Tensor):
 
     abs_diff = tf_math.abs(ground_truth - predicted)
 
-    div_np = np.array(tensorflow.get_static_value(abs_diff / ground_truth))
+    div_np = np.array(tf.get_static_value(abs_diff / ground_truth))
 
     if inf in div_np:
         print('\nabs_diff / ground_truth:', abs_diff / ground_truth)
@@ -67,7 +67,23 @@ def abs_rel(ground_truth: Tensor, predicted: Tensor):
 
 
 def sq_rel(ground_truth: Tensor, predicted: Tensor):
+    gt_np = np.array(tf.get_static_value(ground_truth))
+    pred_np = np.array(tf.get_static_value(ground_truth))
+    print()
+
+    if inf in gt_np:
+        print('\nground_truth:', gt_np)
+
+    if inf in pred_np:
+        print('\npred_inf:', pred_np)
+
     squared_diff = tf_math.squared_difference(ground_truth, predicted)
+
+    sq_np = np.array(tf.get_static_value(squared_diff))
+
+    if inf in sq_np:
+        print('\nsq_np:', sq_np)
+
     return tf_math.reduce_mean(squared_diff / ground_truth)
 
 
@@ -77,12 +93,19 @@ def rmse(ground_truth: Tensor, predicted: Tensor):
 
 
 def rmse_log(ground_truth: Tensor, predicted: Tensor):
-    gt_log = tf_math.log(ground_truth)
-    predicted_log = tf_math.log(predicted)
+    gt_normalized = tf.clip_by_value(ground_truth, clip_value_min=1e-10, clip_value_max=1e10)
+    predicted_normalized = tf.clip_by_value(predicted, clip_value_min=1e-10, clip_value_max=1e10)
+
+    gt_log = tf_math.log(gt_normalized)
+    predicted_log = tf_math.log(predicted_normalized)
+
     squared_log_error = tf_math.squared_difference(gt_log, predicted_log)
     return tf_math.sqrt(tf_math.reduce_mean(squared_log_error))
 
 
 def log_10(ground_truth: Tensor, predicted: Tensor):
-    diff_log_10 = log10(ground_truth) - log10(predicted)
+    gt_normalized = tf.clip_by_value(ground_truth, clip_value_min=1e-10, clip_value_max=1e10)
+    predicted_normalized = tf.clip_by_value(predicted, clip_value_min=1e-10, clip_value_max=1e10)
+
+    diff_log_10 = log10(gt_normalized) - log10(predicted_normalized)
     return tf_math.reduce_mean(tf_math.abs(diff_log_10))
