f7ad889541bc6167562aec9f9c6965abd3925e67
duraes-antonio <garotoseis@gmail.com>
2022-10-23 01:43:31 -0300

    REFACTOR (persistir resultados): Converter métodos estáticos que obtém info. do hardware p/ funções

--- infra/keras/callbacks/save_execution.py
+++ infra/keras/callbacks/save_execution.py
@@ -6,8 +6,22 @@ from tensorflow.python.client import device_lib
 
 from domain.models.test_case.test_case import TestCaseState
 from domain.models.test_case.test_case_execution_history import TestCaseExecutionHistory
+from domain.models.train_result import TrainResult
 from domain.services.test_case_execution_service import TestCaseExecutionService
 from domain.services.test_case_service import TestCaseService
+
+
+def __get_cpu_info__() -> dict:
+    cpu_info = get_cpu_info()
+    ignored_keys = {k for k in cpu_info if 'hz_' in k or k == 'flags'}
+    cpu_info = {k: cpu_info[k] for k in cpu_info if k not in ignored_keys}
+    return cpu_info
+
+
+def __get_gpu_info__() -> Optional[dict]:
+    devices = device_lib.list_local_devices()
+    gpus = [device for device in devices if device.device_type == "GPU"]
+    return gpus if gpus else None
 
 
 class ExecutionSave(callbacks.Callback):
@@ -26,7 +40,7 @@ class ExecutionSave(callbacks.Callback):
         self.test_case_id = test_case_id
         self.start_epoch = start_epoch
 
-    def on_epoch_end(self, epoch_index: int, logs=None):
+    def on_epoch_end(self, epoch_index: int, logs: Optional[TrainResult] = None):
         epoch = self.start_epoch + epoch_index
         epoch_suffix = f'_epoch-{epoch}'
         new_filename = self.filename + epoch_suffix
@@ -38,8 +52,9 @@ class ExecutionSave(callbacks.Callback):
                 'model_id': file_id,
                 'model_name': new_filename,
                 'last_epoch': epoch,
-                'cpu_description': str(self.__get_cpu_info__()),
-                'gpu_description': str(self.__get_gpu_info__()),
+                'cpu_description': str(__get_cpu_info__()),
+                'gpu_description': str(__get_gpu_info__()),
+                'result': logs
             }
             self.execution_service.save(data)
             self.test_case_service.update_state(self.test_case_id, TestCaseState.Busy)
@@ -49,14 +64,3 @@ class ExecutionSave(callbacks.Callback):
             print(error)
             print(f"\n\nError on update execution history: '{error}'")
             exit(1)
-
-    def __get_cpu_info__(self) -> dict:
-        cpu_info = get_cpu_info()
-        ignored_keys = {k for k in cpu_info if 'hz_' in k or k == 'flags'}
-        cpu_info = {k: cpu_info[k] for k in cpu_info if k not in ignored_keys}
-        return cpu_info
-
-    def __get_gpu_info__(self) -> Optional[dict]:
-        devices = device_lib.list_local_devices()
-        gpus = [device for device in devices if device.device_type == "GPU"]
-        return gpus if gpus else None
