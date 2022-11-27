3e5216555c8c0e3472771dfdb905ca8aae92b667
duraes-antonio <garotoseis@gmail.com>
2022-11-27 01:23:14 -0300

    Revert "REFACTOR: Realizar redimensionamento com imutils"
    This reverts commit 17b7a53ec6919214d36eb7b95cb56126177a97b6.

--- infra/util/preprocessing.py
+++ infra/util/preprocessing.py
@@ -1,7 +1,6 @@
 from typing import Tuple
 
 import cv2
-import imutils
 from numpy import ndarray, reshape
 
 DEFAULT_HEIGHT = 256
@@ -15,7 +14,6 @@ def normalize_img(img: ndarray) -> ndarray:
 def preprocess_image(img_path: str, size: Tuple[int, int] = DEFAULT_SIZE) -> ndarray:
     image: ndarray = cv2.imread(img_path)
     image = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
-    image = imutils.resize(image, height=size[1])
     image = image.astype("float")
     image = normalize_img(image)
     return image
@@ -23,7 +21,7 @@ def preprocess_image(img_path: str, size: Tuple[int, int] = DEFAULT_SIZE) -> nda
 
 def preprocess_depth_map(depth_map_path: str, size: Tuple[int, int] = DEFAULT_SIZE) -> ndarray:
     depth_map: ndarray = cv2.imread(depth_map_path, cv2.COLOR_BGR2GRAY)
-    depth_map = imutils.resize(depth_map, height=size[1])
+    depth_map = cv2.resize(depth_map, size, interpolation=cv2.INTER_AREA)
     width, height = size
 
     depth_map = depth_map.astype("float")
