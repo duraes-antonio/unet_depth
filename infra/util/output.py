ad655319721cf78e63a4ea9c98b8834a2b68f3e6
duraes-antonio <garotoseis@gmail.com>
2022-11-29 02:35:14 -0300

    FEAT: Adicionar notebook para comparação qualitativa de resultados

--- infra/util/output.py
+++ infra/util/output.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from typing import Tuple
 
 import matplotlib.pyplot as plot
 import numpy
 from numpy import ndarray
 from tensorflow import keras
 
-from domain.models.data.data_generator import NyuV2Generator
 from domain.models.test_case.test_case import TestCase
 from infra.util.dataset import PathPairs
+from infra.util.preprocessing import preprocess_image, preprocess_depth_map
 
 
 def print_test_case(test_case: TestCase):
@@ -27,6 +28,7 @@ Pesos imagenet: {test_case['use_imagenet_weights']}
 def plot_image_comparison(
         model: keras.Model,
         xy_path_pairs: PathPairs,
+        image_size: Tuple[int, int] = (256, 256)
 ):
     """
     Plot input image, prediction, ground truth side by side
@@ -34,31 +36,34 @@ def plot_image_comparison(
     :param xy_path_pairs: List of path pairs (input path, ground truth path)
     """
     images_name = [Path(x_path).name for x_path, y_path in xy_path_pairs]
-    test_generator = NyuV2Generator(xy_path_pairs, batch_size=1, shuffle=False)
     color_map = plot.get_cmap('inferno_r')
+    width, height = image_size
 
     def plot_depth_map(depth_map: ndarray, title: str, col_number: int):
         _depth_map = numpy.squeeze(depth_map, axis=-1)
         plot_axis = plot.subplot(1, 3, col_number)
-        plot.imshow(_depth_map, cmap=color_map)
         plot_axis.set_title(title)
+        plot.imshow(_depth_map, cmap=color_map)
 
-    for image_name, (x, y) in zip(images_name, test_generator):
-        x, y = numpy.squeeze(x), numpy.squeeze(y)
-
+    for index, (x_path, y_path) in enumerate(xy_path_pairs):
         plot.figure(figsize=(16, 16), dpi=72)
-        predicted = model.predict(x)
+
+        x = numpy.empty((1, width, height, 3))
+        x[0] = preprocess_image(x_path)
 
         # Imagem original
+        image_name = images_name[index]
         input_axis = plot.subplot(1, 3, 1)
-        plot.imshow(x)
-        input_axis.set_title('Input')
+        input_axis.set_title(f'Input ({image_name})')
+        plot.imshow(x[0])
 
         # Predição
-        plot_depth_map(predicted, 'Prediction', 2)
+        predicted = model.predict(x)
+        plot_depth_map(predicted[0], 'Prediction', 2)
 
         # Ground truth
-        plot_depth_map(y, image_name, 3)
+        y = preprocess_depth_map(y_path)
+        plot_depth_map(y, 'Ground Truth', 3)
 
         plot.show()
 
